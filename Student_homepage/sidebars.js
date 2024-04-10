a0ceffd25d80d0b9931533cc32cd1d2d194f9b68
Happy-Pop <116824432+Happy-Pop@users.noreply.github.com>
2024-04-10 23:47:03 +0800

    Add files via upload

--- Student_homepage/sidebars.js
+++ Student_homepage/sidebars.js
@@ -9,3 +9,43 @@ document.getElementById("confirmSignOut").addEventListener("click", function() {
 document.getElementById("cancelSignOut").addEventListener("click", function() {
     document.getElementById("confirmationModal").style.display = "none";
 });
+
+// Example JavaScript code to open/close the modal and populate the table
+document.getElementById('Check_Grades').onclick = function() {
+    showModal();
+};
+
+document.querySelector('.close').onclick = function() {
+    closeModal();
+};
+
+function showModal() {
+    fetch('exam.json')
+    .then(response => response.json()) // 解析响应为JSON
+    .then(data => {
+        const tableBody = document.getElementById('gradesTable').getElementsByTagName('tbody')[0];
+        tableBody.innerHTML = ""; // 清空现有的表格数据
+        
+        // 使用获取到的数据填充表格
+        data.forEach(record => {
+            let row = `<tr>
+                <td>${record.Name}</td>
+                <td>${record.Time}</td>
+                <td>${record.Grade}</td>
+            </tr>`;
+            tableBody.innerHTML += row;
+        });
+
+        document.getElementById('gradesModal').style.display = 'block';
+    })
+    .catch(error => {
+        console.error('Error fetching the JSON:', error);
+        // 处理错误情况，比如显示一条消息告知用户发生了错误
+    });
+}
+
+function closeModal() {
+    document.getElementById('gradesModal').style.display = 'none';
+}
+
+
